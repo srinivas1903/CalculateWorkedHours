f163d24359a40be3caf98551354e6b3dba9c931b
root <root@localhost.localdomain>
2017-10-28 20:41:47 +0530

    Added start end result time

--- app/src/main/java/com/tutorialsquare/calculateworkedhours/MainActivity.java
+++ app/src/main/java/com/tutorialsquare/calculateworkedhours/MainActivity.java
@@ -14,45 +14,80 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-EditText time;
-int start_hour,start_minute, end_hour,end_minute;
+EditText stime,etime;
+    TextView text;
+    Button button;
+int start_hour,start_minute, end_hour,end_minute, result_hours, result_minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //  initiate the edit text
-        time = (EditText) findViewById(R.id.time);
+        stime = (EditText) findViewById(R.id.start);
         // perform click event listener on edit text
-        time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        stime.setOnClickListener(this);
+        etime = (EditText) findViewById(R.id.end);
+        // perform click event listener on edit text
+        etime.setOnClickListener(this);
+        text = (TextView) findViewById(R.id.text1);
+        text.setText("");
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+        switch( id) {
+            case R.id.button1:
+            {
+                if (start_hour > end_hour) {
+                    Toast.makeText(this, "Invalid!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                result_hours = end_hour - start_hour;
+                if ( end_minute < start_minute) {
+                    result_minute = 60 - start_minute + end_minute;
+                    result_hours -= 1;
+                }
+                else
+                    result_minute = end_minute - start_minute;
+
+                text.setText("hours "+result_hours+" "+"minutes "+result_minute);
+                break;
+            }
+            case R.id.start:
+            case R.id.end:
+            {
                 Calendar mcurrentTime = Calendar.getInstance();
                 int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = mcurrentTime.get(Calendar.MINUTE);
                 TimePickerDialog mTimePicker;
-                final int id = v.getId();
+
+
                 mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                                time.setText(selectedHour + ":" + selectedMinute);
-                                if (id == R.id.start) {
-                                    start_hour = selectedHour;
-                                    start_minute = selectedMinute;
-                                }
-                                else
-                                {
-                                    end_hour = selectedHour;
-                                    end_minute = selectedMinute;
-                                }
-                            }
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+
+                        if (id == R.id.start) {
+                            start_hour = selectedHour;
+                            start_minute = selectedMinute;
+                            stime.setText(selectedHour + ":" + selectedMinute);
+                        } else {
+                            end_hour = selectedHour;
+                            end_minute = selectedMinute;
+                            etime.setText(selectedHour + ":" + selectedMinute);
+                        }
+                    }
                 }, hour, minute, true);//Yes 24 hour time
                 mTimePicker.setTitle("Select Time");
                 mTimePicker.show();
-
+                break;
             }
-        });
+            default:
+                break;
+        }
     }
-
 }
